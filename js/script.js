9856986645e1923c0b1dee9dc4a315b3e402987c
irnbru-dev <darina.irnbru@gmail.com>
2017-05-24 00:49:54 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+  	items:1,
+  	nav: true,
+  	navText: ["<img src='img/arrow-left.png'>","<img src='img/arrow-right.png'>"],
+  	center: true,
+  	// dots: true,
+  });
+
+var dot = $('.slideWrap .owl-dot');
+dot.each(function() {
+	var index = $(this).index() + 1;
+  if(index < 7){
+  	$(this).html('0').append(index);
+  }else{
+     $(this).html(index);
+  }
+});
 });
